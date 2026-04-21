a05e027de6d0504be5b03b96092ad7bfc1966fdf
nadjib23-sudo <nadjibdaoudi2005@gmail.com>
2026-04-21 12:18:24 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -384,7 +384,7 @@ export default function App() {
                           <div className={`flex-1 flex flex-col justify-center ${lang === 'ar' ? 'text-right' : 'text-left'}`}>
                             <h6 className="font-bold text-lg mb-1 uppercase tracking-tight">{selectedProduct.name[lang]}</h6>
                             <p className="text-white/40 text-sm mb-2 uppercase tracking-widest font-bold text-[10px]">{selectedProduct.brand} DA </p>
-                            <p className="text-gold font-bold text-xl">{selectedProduct.price.toLocaleString()}  DA/p>
+                            <p className="text-gold font-bold text-xl">{selectedProduct.price.toLocaleString()}  DA</p>
                           </div>
                         </div>
                       </div>
